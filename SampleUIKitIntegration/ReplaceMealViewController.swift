fd48fa9de3a26770529d5c819a0610c4dbfde833
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-06-23 15:05:12 +0100

    fixed meal picker popping twice

--- SampleUIKitIntegration/ReplaceMealViewController.swift
+++ SampleUIKitIntegration/ReplaceMealViewController.swift
@@ -9,41 +9,91 @@ import UIKit
 import SwiftUI
 import MiamIOSFramework
 import CoursesUIOSTemplate
-
+//
+//class ReplaceMealViewController: UIHostingController<CoursesUBudgetPlannerRecipePickerView<
+//        CoursesUBudgetSearch,
+//        CoursesURecipeCard,
+//        CoursesUBudgetPlannerFooter>
+//> {
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        let budgetRecipePicker = CoursesUBudgetPlannerRecipePickerView.init(searchTemplate: CoursesUBudgetSearch(), cardTemplate: CoursesURecipeCard(), stickyFooter: CoursesUBudgetPlannerFooter(), maxBudget: 35.0, onRecipeSelected: {_ in})
+//        super.init(coder: aDecoder, rootView: budgetRecipePicker)
+//    }
+//
+//    override init(rootView: CoursesUBudgetPlannerRecipePickerView<CoursesUBudgetSearch, CoursesURecipeCard, CoursesUBudgetPlannerFooter>) {
+//        super.init(rootView: rootView)
+//    }
+//
+//    public init() {
+//        let budgetRecipePicker = CoursesUBudgetPlannerRecipePickerView.init(searchTemplate: CoursesUBudgetSearch(), cardTemplate: CoursesURecipeCard(), stickyFooter: CoursesUBudgetPlannerFooter(), maxBudget: 35.0, onRecipeSelected: {_ in})
+//        super.init(rootView: budgetRecipePicker)
+//    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        self.title = "Ajouter une idée repas"
+//        let budgetRecipePicker = CoursesUBudgetPlannerRecipePickerView.init(
+//            searchTemplate: CoursesUBudgetSearch(),
+//            cardTemplate: CoursesURecipeCard(),
+//            stickyFooter: CoursesUBudgetPlannerFooter(),
+//            maxBudget: 35.0,
+//            onRecipeSelected: { _ in
+//                DispatchQueue.main.async {
+//                    self.navigationController?.popViewController(animated: true)
+//                }
+//            })
+//        self.rootView = budgetRecipePicker
+//    }
+//}
 class ReplaceMealViewController: UIHostingController<CoursesUBudgetPlannerRecipePickerView<
         CoursesUBudgetSearch,
         CoursesURecipeCard,
         CoursesUBudgetPlannerFooter>
 > {
-    
+
+    var onRecipeSelectedAction: ((String) -> Void)?
+    var isPoppingViewController = false
+
     required init?(coder aDecoder: NSCoder) {
-        let budgetRecipePicker = CoursesUBudgetPlannerRecipePickerView.init(searchTemplate: CoursesUBudgetSearch(), cardTemplate: CoursesURecipeCard(), stickyFooter: CoursesUBudgetPlannerFooter(), maxBudget: 35.0, onRecipeSelected: {_ in})
-        super.init(coder: aDecoder, rootView: budgetRecipePicker)
+        super.init(coder: aDecoder, rootView: ReplaceMealViewController.createRootView())
+        setupRecipeSelectedAction()
     }
-    
+
     override init(rootView: CoursesUBudgetPlannerRecipePickerView<CoursesUBudgetSearch, CoursesURecipeCard, CoursesUBudgetPlannerFooter>) {
-        super.init(rootView: rootView)
+        super.init(rootView: ReplaceMealViewController.createRootView())
+        setupRecipeSelectedAction()
     }
-    
+
     public init() {
-        let budgetRecipePicker = CoursesUBudgetPlannerRecipePickerView.init(searchTemplate: CoursesUBudgetSearch(), cardTemplate: CoursesURecipeCard(), stickyFooter: CoursesUBudgetPlannerFooter(), maxBudget: 35.0, onRecipeSelected: {_ in})
-        super.init(rootView: budgetRecipePicker)
+        super.init(rootView: ReplaceMealViewController.createRootView())
+        setupRecipeSelectedAction()
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Ajouter une idée repas"
-        let budgetRecipePicker = CoursesUBudgetPlannerRecipePickerView.init(
+        self.rootView = ReplaceMealViewController.createRootView(onRecipeSelected: onRecipeSelectedAction)
+    }
+
+    private static func createRootView(onRecipeSelected: ((String) -> Void)? = nil) -> CoursesUBudgetPlannerRecipePickerView<CoursesUBudgetSearch, CoursesURecipeCard, CoursesUBudgetPlannerFooter> {
+        return CoursesUBudgetPlannerRecipePickerView(
             searchTemplate: CoursesUBudgetSearch(),
             cardTemplate: CoursesURecipeCard(),
             stickyFooter: CoursesUBudgetPlannerFooter(),
             maxBudget: 35.0,
-            onRecipeSelected: { _ in
-                // this should add Recipe back to VM?
-                DispatchQueue.main.async {
-                    self.navigationController?.popViewController(animated: true)
-                }
-            })
-        self.rootView = budgetRecipePicker
+            onRecipeSelected: onRecipeSelected ?? { _ in }
+        )
+    }
+
+    private func setupRecipeSelectedAction() {
+        onRecipeSelectedAction = { _ in
+            DispatchQueue.main.async { [weak self] in
+                // because its popping 2 screens at a time 
+                guard let self = self, !self.isPoppingViewController else { return }
+                                self.isPoppingViewController = true
+                                self.navigationController?.popViewController(animated: true)
+            }
+        }
     }
 }
