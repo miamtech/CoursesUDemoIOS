54840a2accbf314e71ef8879bb842ac101d5e119
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-06-24 11:41:44 +0100

    Updated template names for BasketPReview View

--- SampleUIKitIntegration/BasketPreviewViewController.swift
+++ SampleUIKitIntegration/BasketPreviewViewController.swift
@@ -17,17 +17,17 @@ CoursesUMealPlannerBasketPreviewLoading,
     CoursesUMealPlannerBasketPreviewSectionTitle,
     CoursesUMealPlannerBasketPreviewSectionProduct>
 > {
-    
+
     required init?(coder aDecoder: NSCoder) {
         let budgetPreview = CoursesUMealPlannerBasketPreviewView.init(
             loadingTemplate: CoursesUMealPlannerBasketPreviewLoading(),
-            recipeOverview: CoursesUMealPlannerBasketPreviewRecipeOverview(),
-            basketProduct: CoursesUMealPlannerBasketPreviewProduct(),
+            recipeOverviewTemplate: CoursesUMealPlannerBasketPreviewRecipeOverview(),
+            productTemplate: CoursesUMealPlannerBasketPreviewProduct(),
             sectionTitleTemplate: CoursesUMealPlannerBasketPreviewSectionTitle(),
             sectionProductTemplate: CoursesUMealPlannerBasketPreviewSectionProduct(), validateRecipes: {})
         super.init(coder: aDecoder, rootView: budgetPreview)
     }
-    
+
     override init(rootView: CoursesUMealPlannerBasketPreviewView<
                   CoursesUMealPlannerBasketPreviewLoading,
                   CoursesUMealPlannerBasketPreviewRecipeOverview,
@@ -36,12 +36,12 @@ CoursesUMealPlannerBasketPreviewLoading,
                   CoursesUMealPlannerBasketPreviewSectionProduct>) {
         super.init(rootView: rootView)
     }
-    
+
     public init() {
         let budgetPreview = CoursesUMealPlannerBasketPreviewView.init(
             loadingTemplate: CoursesUMealPlannerBasketPreviewLoading(),
-            recipeOverview: CoursesUMealPlannerBasketPreviewRecipeOverview(),
-            basketProduct: CoursesUMealPlannerBasketPreviewProduct(),
+            recipeOverviewTemplate: CoursesUMealPlannerBasketPreviewRecipeOverview(),
+            productTemplate: CoursesUMealPlannerBasketPreviewProduct(),
             sectionTitleTemplate: CoursesUMealPlannerBasketPreviewSectionTitle(),
             sectionProductTemplate: CoursesUMealPlannerBasketPreviewSectionProduct(),validateRecipes: {})
         super.init(rootView: budgetPreview)
@@ -52,8 +52,8 @@ CoursesUMealPlannerBasketPreviewLoading,
         self.title = "Mon assistant Budget repas"
         let budgetPreview = CoursesUMealPlannerBasketPreviewView.init(
             loadingTemplate: CoursesUMealPlannerBasketPreviewLoading(),
-            recipeOverview: CoursesUMealPlannerBasketPreviewRecipeOverview(),
-            basketProduct: CoursesUMealPlannerBasketPreviewProduct(),
+            recipeOverviewTemplate: CoursesUMealPlannerBasketPreviewRecipeOverview(),
+            productTemplate: CoursesUMealPlannerBasketPreviewProduct(),
             sectionTitleTemplate: CoursesUMealPlannerBasketPreviewSectionTitle(),
             sectionProductTemplate: CoursesUMealPlannerBasketPreviewSectionProduct(),
             validateRecipes: {
@@ -65,3 +65,66 @@ CoursesUMealPlannerBasketPreviewLoading,
         // Do any additional setup after loading the view.
     }
 }
+//
+//
+//class BasketPreviewViewController: UIHostingController<MealPlannerBasketPreviewView<
+//CoursesUMealPlannerBasketPreviewLoading,
+//    CoursesUMealPlannerBasketPreviewRecipeOverview,
+//    CoursesUMealPlannerBasketPreviewProduct,
+//    CoursesUMealPlannerBasketPreviewSectionTitle,
+//    CoursesUMealPlannerBasketPreviewSectionProduct>
+//> {
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        let budgetPreview = MealPlannerBasketPreviewView.init(
+//            loadingTemplate: CoursesUMealPlannerBasketPreviewLoading(),
+//            recipeOverviewTemplate: CoursesUMealPlannerBasketPreviewRecipeOverview(),
+//            productTemplate: CoursesUMealPlannerBasketPreviewProduct(),
+//            sectionTitleTemplate: CoursesUMealPlannerBasketPreviewSectionTitle(),
+//            sectionProductTemplate: CoursesUMealPlannerBasketPreviewSectionProduct()
+////            ,
+////            validateRecipes: {}
+//        )
+//        super.init(coder: aDecoder, rootView: budgetPreview)
+//    }
+//
+//    override init(rootView: MealPlannerBasketPreviewView<
+//                  CoursesUMealPlannerBasketPreviewLoading,
+//                  CoursesUMealPlannerBasketPreviewRecipeOverview,
+//                  CoursesUMealPlannerBasketPreviewProduct,
+//                  CoursesUMealPlannerBasketPreviewSectionTitle,
+//                  CoursesUMealPlannerBasketPreviewSectionProduct>) {
+//        super.init(rootView: rootView)
+//    }
+//
+//    public init() {
+//        let budgetPreview = MealPlannerBasketPreviewView.init(
+//            loadingTemplate: CoursesUMealPlannerBasketPreviewLoading(),
+//            recipeOverviewTemplate: CoursesUMealPlannerBasketPreviewRecipeOverview(),
+//            productTemplate: CoursesUMealPlannerBasketPreviewProduct(),
+//            sectionTitleTemplate: CoursesUMealPlannerBasketPreviewSectionTitle(),
+//            sectionProductTemplate: CoursesUMealPlannerBasketPreviewSectionProduct()
+////            validateRecipes: {}
+//        )
+//        super.init(rootView: budgetPreview)
+//    }
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        self.title = "Mon assistant Budget repas"
+//        let budgetPreview = MealPlannerBasketPreviewView.init(
+//            loadingTemplate: CoursesUMealPlannerBasketPreviewLoading(),
+//            recipeOverviewTemplate: CoursesUMealPlannerBasketPreviewRecipeOverview(),
+//            productTemplate: CoursesUMealPlannerBasketPreviewProduct(),
+//            sectionTitleTemplate: CoursesUMealPlannerBasketPreviewSectionTitle(),
+//            sectionProductTemplate: CoursesUMealPlannerBasketPreviewSectionProduct()
+////            validateRecipes: {
+////                DispatchQueue.main.async {
+////                    self.navigationController?.pushViewController(RecapPurchaseViewController(), animated: true)
+////                }
+////            }
+//        )
+//        self.rootView = budgetPreview
+//        // Do any additional setup after loading the view.
+//    }
+//}
