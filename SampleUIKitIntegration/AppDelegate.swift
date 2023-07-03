873e6e14c10d2fb36c7885a4c1ed0875d6bb8ea5
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-07-03 16:41:19 +0100

    added framework for SPM, added BasketHandler to init

--- SampleUIKitIntegration/AppDelegate.swift
+++ SampleUIKitIntegration/AppDelegate.swift
@@ -16,13 +16,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         LogHandler.companion.info("Are you ready ? \(ContextHandlerInstance.shared.instance.isReady())")
-            PointOfSaleHandler.shared.updateStoreId(storeId: "25910")
-            PointOfSaleHandler.shared.setSupplierOrigin(origin:"app.coursesu.com")
-            PointOfSaleHandler.shared.setSupplier(supplierId: 7)
-//            PointOfSaleHandler.shared.setSupplier(supplierId: 9)
-//            PointOfSaleHandler.shared.setSupplierOrigin(origin: "www.franprix.fr-dev")
-            //PointOfSaleHandler.shared.isAvailable = false
+        
+//        let basketHandler = BasketHandlerInstance.shared.instance
+        BasketHandlerInstance.shared.instance.setListenToRetailerBasket(func: {})
+        BasketHandlerInstance.shared.instance.setPushProductsToRetailerBasket(func: {_ in})
+        BasketHandlerInstance.shared.instance.pushProductsToMiamBasket(retailerBasket: [])
+        
+        PointOfSaleHandler.shared.updateStoreId(storeId: "25910")
+        PointOfSaleHandler.shared.setSupplierOrigin(origin:"app.coursesu.com")
+        PointOfSaleHandler.shared.setSupplier(supplierId: 7)
+
             UserHandler.shared.updateUserId(userId: "ed0a471a4bdc755664db84068119144b3a1772d8a6911057a0d6be6a3e075120")
+        // resets grocery cart
+        GroceriesListHandler.shared.resetGroceriesList()
             
         return true
     }
