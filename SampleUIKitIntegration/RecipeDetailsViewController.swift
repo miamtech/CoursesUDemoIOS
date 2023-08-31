bb6825a4a9585d5b7e5bcb36d2ea59cd9e1aa20c
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2023-08-31 20:50:46 +0100

    remove comments

--- SampleUIKitIntegration/RecipeDetailsViewController.swift
+++ SampleUIKitIntegration/RecipeDetailsViewController.swift
@@ -10,60 +10,6 @@ import SwiftUI
 import MiamIOSFramework
 import CoursesUxMiamFramework
 
-//class RecipeDetailsViewController: UIHostingController<
-//    RecipeDetailsViewTemplate<
-//        CoursesURecipeDetailsHeaderView,
-//        CoursesURecipeDetailsIngredientsView,
-//        CoursesURecipeDetailsStepsView,
-//        CoursesURecipeDetailsFooterView>
-//    > {
-//
-//        required init?(coder aDecoder: NSCoder) {
-//                 super.init(coder: aDecoder)
-//             }
-//
-//             override init(rootView: RecipeDetailsViewTemplate<
-//                           CoursesURecipeDetailsHeaderView,
-//                           CoursesURecipeDetailsIngredientsView,
-//                           CoursesURecipeDetailsStepsView,
-//                           CoursesURecipeDetailsFooterView>
-//                       ) {
-//                 super.init(rootView: rootView)
-//             }
-//
-//             public init() {
-//                 let itemSelector = RecipeDetailsViewTemplate.init(
-//                    headerContent: CoursesURecipeDetailsHeaderView(),
-//                    ingredientsContent: CoursesURecipeDetailsIngredientsView(),
-//                    stepsContent: CoursesURecipeDetailsStepsView(),
-//                    footerContent: CoursesURecipeDetailsFooterView(),
-//                    recipeId: "",
-//                    isForMealPlanner: true,
-//                    sponsorDetailsTapped: {_ in},
-//                    close: {})
-//                 super.init(rootView: itemSelector)
-//             }
-//
-//             override func viewDidLoad() {
-//                 let recipeId = UserDefaults.standard.value(forKey: "miam_mealplanner_recipeId") as? String ?? ""
-//                 self.title = "Catalogue"
-//                 super.viewDidLoad()
-//                 let itemSelector = RecipeDetailsViewTemplate.init(
-//                    headerContent: CoursesURecipeDetailsHeaderView(),
-//                    ingredientsContent: CoursesURecipeDetailsIngredientsView(),
-//                    stepsContent: CoursesURecipeDetailsStepsView(),
-//                    footerContent: CoursesURecipeDetailsFooterView(),
-//                    recipeId: recipeId,
-//                    isForMealPlanner: true,
-//                    sponsorDetailsTapped: {_ in},
-//                    close: {
-//                         self.navigationController?.popViewController(animated: true)
-//                 })
-//                 self.rootView = itemSelector
-//                 // Do any additional setup after loading the view.
-//             }
-//         }
-
 class RecipeDetailsViewController: UIViewController {
     deinit {
         print("deinit: RecipeDetailsViewController is being deallocated")
